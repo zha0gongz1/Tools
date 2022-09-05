36df96ce8c82b0610b1c36d23ac66eff587e10c5
赵公子 <zha0gongz1@protonmail.com>
2022-09-05 12:13:20 +0800

    Add files via upload

---
+++ Windows/DelDefenderRecord.bat
@@ -0,0 +1,12 @@
+@echo off
+set "folder=C:\ProgramData\Microsoft\Windows" "Defender\Scans\History\Service\DetectionHistory"
+if not exist "%folder%" (echo;"%folder%" no found&goto end)
+if "%folder:~-1%" equ "\" (set "folder=%folder:~,-1%")
+(forfiles /p %folder% /s /d %date:~0,10% /c "cmd /c echo @path-@fdate-@ftime >C:\Temp\record.txt"
+forfiles /p %folder% /s /d %date:~0,10% /c "cmd /c del /q @file"
+::如果只显示目录名，则为空文件夹，说明最新记录文件已删除
+forfiles /p %folder% /s /d %date:~0,10% /c "cmd /c echo @file" 
+)
+exit
+
+
