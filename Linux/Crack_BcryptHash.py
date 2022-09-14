7aae5ca0eec5d337cf8049338ce1755f84e2fa2e
赵公子 <zha0gongz1@protonmail.com>
2022-09-14 11:49:41 +0800

    Update Crack_BcryptHash.py

--- Linux/Crack_BcryptHash.py
+++ Linux/Crack_BcryptHash.py
@@ -7,20 +7,17 @@ def pingo(iteration,
           decimals=1,
           length=100,
           fill='█'):
-    """
-    Print
-    """
-    length = length - (len(prefix) + len(suffix) + 10)
+
+    length = length - (len(prefix) + len(suffix) +10)
+    iteration += 1
     percent = ("{0:." + str(decimals) + "f}").format(
-        100 * (iteration / float(total)))
+        100 * (iteration/ float(total)))
     fillLength = int(length * iteration // total)
-    bar = fill * fillLength + '-' * (length - fillLength)
+    bar = fill * fillLength + '-' * (length - fillLength-1)
     print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end='\r')
-    if iteration == total:
-        print()
 
 
-text_file = open("pass.txt", "r", encoding="cp437")
+text_file = open("pass.txt", "r", encoding="utf8")
 words = text_file.read().splitlines()
 
 hash = input('Hash to crack: ')
@@ -30,8 +27,9 @@ length = len(words)
 correct_word = ""
 found = 0
 for (index,word) in enumerate(words):
-	pingo(index, length, prefix='Wait:', suffix='Words complete from the list')
+	
 	correct = bcrypt.checkpw(word.encode('utf8'), hash.encode('utf8'))
+	pingo(index, length, prefix='Wait:', suffix='Words complete from the list')
 	if (correct):
 		correct_word = word
 		found += 1
@@ -42,5 +40,3 @@ if (found == 1):
     print("[*]Results:", correct_word)
 else:
     print("\n[-]Unfortunately, password not found.")
-
-
