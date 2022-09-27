78ec93ec700f2c56d14e84c178ba2093de2b2abc
赵公子 <zha0gongz1@protonmail.com>
2022-09-27 18:51:53 +0800

    get web title

--- Linux/httpscan.sh
+++ Linux/httpscan.sh
@@ -12,11 +12,12 @@ fi
 
 main(){
 	output=$BASE_IP"_result"
-	status_code=$(curl -H 'Accept-Language: en-US,en;q=0.5' -A 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko' -o /dev/null -s -w '%{http_code}' -m 10 http://$subip:80/)
+	status_code=$(curl -H 'Accept-Language: en-US,en;q=0.5' -A 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko' -I -s -m 6 http://$subip:80/ | grep HTTP/ | awk {'print $2'})
 	#echo $status_code
+	title=$(curl -H 'Accept-Language: en-US,en;q=0.5' -A 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko' -L -s -m 10 http://$subip:80/ | grep -iPo '(?<=<title>)(.*)(?=</title>)')
 	if [[ "$status_code" -ge 200 ]]; then
   		echo -e "\033[92m[+]"$subip" status_code:"$status_code"\033[0m"
-		echo "[+]"$subip" status_code:"$status_code >>$output
+		echo -e "IP:"$subip"\t||status_code:"$status_code"\t||web_title:"$title >>$output
 	#else
   		#echo -e "\033[91m[-]"$subip" not exist web service\033[0m"
 	fi
