efaccc11549eda8ca75b2b2548871b211f08f40f
赵公子 <zha0gongz1@protonmail.com>
2023-04-25 11:10:09 +0800

    Update subdomain.sh

--- Linux/subdomain.sh
+++ Linux/subdomain.sh
@@ -7,11 +7,14 @@
 # subfinder
 # oneforall
 # gau
+# katana
 # nuclei && nuclei-templates
+# -------------------------------------------------------------------
+# Output:$domain-subdoamin.txt && $domain-links.txt && $domain-nuclei-res.txt
 ######################################################################
-
 domain=$1
 touch NewSubDomainsBrute.txt oneforall.txt subfinder.txt
+
 # 使用subDomainsBrute进行子域名枚举
 echo -e "\033[5m\033[1;33m[+]Start processing subDomainsBrute...\033[0m"
 python3 /root/subDomainsBrute/subDomainsBrute.py  $domain -o subDomainsBrute.txt
@@ -28,18 +31,24 @@ python3 /root/OneForAll/oneforall.py --target $domain --req False run
 #子域结果处理
 echo -e "\033[5m\033[0;36m[+]Start sorting results...\033[0m"
 cat /root/OneForAll/results/$domain.csv |awk -F ',' 'NR>1 {print $6}' | sort | uniq | tee oneforall.txt
-
 sed -r 's/([0-9]{1,3}\.){3}[0-9]{1,3}//g' subDomainsBrute.txt > NewSubDomainsBrute.txt | cat NewSubDomainsBrute.txt subfinder.txt oneforall.txt | sort | uniq > $domain-subdoamin.txt
 
-sleep 1
-# GAU
+# 链接爬取
+# gau
 echo -e "\033[5m\033[0;36m[+]Start fetching all the urls...\033[0m"
 #cat $domain_subdoamin.txt | /home/kali/go/bin/waybackurls | /home/kali/go/bin/unfurl -u domains > $domain_waybackurls.txt
 cat $domain-subdoamin.txt | gau --threads 50 > $domain-gau.txt
-rm subDomainsBrute.txt NewSubDomainsBrute.txt subfinder.txt oneforall.txt
+sleep 1
+#Katana
+cat $domain-subdoamin.txt | katana -iqp -kf all > $domain-katana.txt
+sleep 1
+cat $domain-katana.txt $domain-gau.txt | sort | uniq > $domain-links.txt
+
+rm subDomainsBrute.txt NewSubDomainsBrute.txt subfinder.txt oneforall.txt $domain-gau.txt $domain-katana.txt
 
 # Nuclei
 echo -e "\033[5m\033[0;36m[+]Start detecting vulns...\033[0m"
-nuclei -t /root/nuclei-templates/ -severity critical,high,medium,low -l $domain-subdoamin.txt -bs 50 -c 50 -rl 120 -nc -o $domain-nuclei-res.txt
+nuclei -t /root/nuclei-templates/ -severity critical,high,medium,low -l $domain-subdoamin.txt -bs 30 -c 30 -rl 120 -nc -o $domain-nuclei-res.txt
 
-echo -e "\033[5m\033[1;36m[+]Success! Good luck!\033[0m"
+echo "Runtime: $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
+echo -e "\033[5m\033[1;36m[+]Success! Good luck for penetration! \033[0m"
